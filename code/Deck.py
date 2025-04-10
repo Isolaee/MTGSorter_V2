cfa05e77e6389958ff7892ea6949c8cfec9a5ece
isolaee <eero.isola@gmail.com>
2025-04-10 21:42:01 +0300

    Abstract MTGDeck and UML update for naming

---
+++ code/Deck.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class Deck(ABC):
+    """Abstract base class for a Deck."""
+
+    def __init__(
+        self,
+        name: str,
+    ):
+
+        self.name = name
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """Return a string representation of the deck."""
+        pass
--- code/MTGDeck.py
+++ code/MTGDeck.py
@@ -1,16 +1,31 @@
-from typing import Generic, TypeVar, List
+from typing import TypeVar
 from PlayingCard import PlayingCard  # Assuming MTGCard inherits from PlayingCard
+from Deck import Deck
+from abc import ABC, abstractmethod
 
 # Define a TypeVar for the card type
 T = TypeVar("T", bound=PlayingCard)  # T must be a subclass of PlayingCard
 
 
-class MTGdeck(Generic[T]):
+class MTGdeck(Deck, ABC):
     """Class representing a deck of Magic: The Gathering cards."""
 
-    def __init__(self):
-        self.cards: List[T] = []  # List to hold cards of type T
+    def __init__(self, name: str, scryfallStatic: str) -> None:
+        super().__init__(name)
+        self.name = name
+        self.scryfallStatic = scryfallStatic
 
+    @abstractmethod
+    def getName(self) -> str:
+        """Return the name of the deck."""
+        return self.name
+
+    @abstractmethod
+    def getScryfallStatic(self) -> str:
+        """Return the Scryfall static URL."""
+        return self.scryfallStatic
+
+    @abstractmethod
     def add_card(self, card: T) -> None:
         """Add a card to the deck."""
         if len(self.cards) < 60:
@@ -18,12 +33,14 @@ class MTGdeck(Generic[T]):
         else:
             raise ValueError("Deck already contains 60 cards.")
 
+    @abstractmethod
     def shuffle(self) -> None:
         """Shuffle the deck."""
         import random
 
         random.shuffle(self.cards)
 
+    @abstractmethod
     def draw_card(self) -> T:
         """Draw a card from the top of the deck."""
         if self.cards:
