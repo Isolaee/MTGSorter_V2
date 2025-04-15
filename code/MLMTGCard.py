29f0f80788802c8bd0e3d1b0155940cb0c70b01f
isolaee <eero.isola@gmail.com>
2025-04-15 21:52:22 +0300

    Added basic MLMTGCard class for Machine learning. Added Word2Vec model and python code for vectorizing character data. That model is trained on MTG texts.

---
+++ code/MLMTGCard.py
@@ -0,0 +1,39 @@
+from MTGCard import MTGCard
+
+
+class MLMTGCard(MTGCard):
+    """Class for a Magic: The Gathering card with machine learning features."""
+
+    def __init__(
+        self,
+        name,
+        manacost,
+        cmc,
+        colors,
+        power,
+        toughness,
+        oracleText,
+        loyalty,
+        typeline,
+        cardType,
+    ):
+
+        super().__init__(
+            name,
+            manacost,
+            cmc,
+            colors,
+            power,
+            toughness,
+            oracleText,
+            loyalty,
+            typeline,
+            cardType,
+            colorIdentity=None,
+            cardFaces=None,
+            allParts=None,
+            layout=None,
+            artist=None,
+            scryfallid=None,
+            legalities=None,
+        )
